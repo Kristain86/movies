86c9ff4280f1586293949bd9b2900c887e091c69
Cristian Sagula <csagula@litebox.ai>
2024-06-15 11:31:50 -0300

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
+            sadasd
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
